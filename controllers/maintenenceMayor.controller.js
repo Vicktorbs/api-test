f3ba8d7ac2351c4b94000cbddb0147b2ad824d90
Victor Bolaños Solís <viktorb.132@gmail.com>
2020-11-14 00:59:32 -0600

    Correccionde de mantenimiento mayor

--- controllers/maintenenceMayor.controller.js
+++ controllers/maintenenceMayor.controller.js
@@ -14,206 +14,57 @@ MMMA_M01.editMMMA_M01Data = async (req, res) => {
         numberOfMaterial: req.body.numberOfMaterialInputs,
         arr: req.body.materials
     }
+    let equipos = {
+        numberOfEquipo: req.body.numberOfEquipmentInputs,
+        arr: req.body.equipments
+    }
     const MMMA_M01 = {
-        noInspeccion: req.body.inspeccion || '',
-        noTren: req.body.tren || '',
-        kilometraje: req.body.distance || 0,
-        fechaInicio: req.body.startTime || '',
-        fechaTerminacion: req.body.endTime || '',
- 
-        colocarSm1: req.body || false,
-        observacionesCs: req.body || '',
-
-        retirarTm1: req.body || false,
-        observacionesRt: req.body || '',
-
-        desconectarMm1: req.body || false,
-        observacionesDm: req.body || '',
-
-        retirarTFm1: req.body || false,
-        observacionesRtf: req.body || '',
-
-        bajarPm1: req.body || false,
-        observacionesBp: req.body || '',
-
-        //2. MANTENIMIENTO DEL PANTOGRAFO
-        conjuntoAR: req.body || false,
-        conjuntoAL: req.body || false,
-        conjuntoAC: req.body || false,
-        observacionesCam: req.body || '',
-
-        conjuntoBR: req.body || false,
-        conjuntoBL: req.body || false,
-        conjuntoBE: req.body || false,
-        conjuntoBC: req.body || false,
-        observacionesCb: req.body || '',
-
-        conjuntoBIR: req.body || false,
-        conjuntoBIL: req.body || false,
-        conjuntoBIE: req.body || false,
-        conjuntoBIC: req.body || false,
-        observacionesCbi: req.body || '',
-
-        conjuntoER: req.body || false,
-        conjuntoEL: req.body || false,
-        conjuntoEE: req.body || false,
-        conjuntoEC: req.body || false,
-        observacionesCe: req.body || '',
-
-        conjuntoBER: req.body || false,
-        conjuntoBEL: req.body || false,
-        conjuntoBEE: req.body || false,
-        conjuntoBEC: req.body || false,
-        observacionesCbe: req.body || '',
-
-        conjuntoCR: req.body || false,
-        conjuntoCL: req.body || false,
-        conjuntoCE: req.body || false,
-        conjuntoCC: req.body || false,
-        observacionesCc: req.body || '',
-
-        conjuntoAC: req.body || false,
-        observacionesCa: req.body || '',
-
-        conjuntoMR: req.body || false,
-        conjuntoML: req.body || false,
-        conjuntoME: req.body || false,
-        conjuntoMC: req.body || false,
-        observacionesCm: req.body || '',
-
-        conjuntoMSR: req.body || false,
-        conjuntoMSL: req.body || false,
-        conjuntoMSC: req.body || false,
-        observacionesCms: req.body || '',
-
-        conjuntoVR: req.body || false,
-        conjuntoVL: req.body || false,
-        conjuntoVE: req.body || false,
-        conjuntoVC: req.body || false,
-        observacionesCv: req.body || '',
-
-        //CONEXIONES FLEXIBLES
-        verificarE8re: req.body || false,
-        observacionesVe8: req.body || '',
-
-        verificarECre: req.body || false,
-        observacionesVec: req.body || '',
-
-        aplicarGre: req.body || false,
-        observacionesAg: req.body || '',
-
-        colocarMCre: req.body || false,
-        observacionesCmc: req.body || '',
-
-        aplicarPre: req.body || false,
-        observacionesAp: req.body || '',
-
-        //ARQUET NUEVO
-        verificarVre: req.body || false,
-        observacionesVv: req.body || '',
-
-        separacionBre: req.body || false,
-        observacionesSb: req.body || '',
-
-        alizarAre: req.body || false,
-        observacionesAa: req.body || '',
-
-        verificarTre: req.body || false,
-        observacionesVt: req.body || '',
-
-        //MOTOR NEUMATICO(CON PRESION DE AIRE REGULADA)
-        presionAre: req.body || false,
-        observacionesPa: req.body || '',
-
-        presionRre: req.body || false,
-        observacionesPr: req.body || '',
-
-        recorridoPre: req.body || false,
-        observacionesRp: req.body || '',
-
-        alimentarCre: req.body || false,
-        observacionesAc: req.body || '',
-
-        //LUBRICACION
-        codigoCm: req.body || '',
-        descipcionCm: req.body || '',
-        reCm: req.body || false,
-        observacionesCmec: req.body || '',
-
-        codigoCep: req.body || '',
-        descipcionCep: req.body || '',
-        reCep: req.body || false,
-        observacionesCep: req.body || '',
-
-        codigoEn: req.body || '',
-        descipcionEn: req.body || '',
-        reEn: req.body || false,
-        observacionesEn: req.body || '',
-
-        codigoPe: req.body || '',
-        descipcionPe: req.body || '',
-        rePe: req.body || false,
-        observacionesPe: req.body || '',
-
-        //3. MONTAJE DE PANTOGRAFO
-        subirPm1: req.body || false,
-        observacionesSp: req.body || '',
-
-        colocarTm1: req.body || false,
-        observacionesCt: req.body || '',
-
-        conectarMm1: req.body || false,
-        observacionesCm: req.body || '',
-
-        conectarCm1: req.body || false,
-        observacionesCc: req.body || '',
-
-        realizarAm1: req.body || false,
-        observacionesRa: req.body || '',
-
-        //PARES DE APRIETE
-        re1: req.body || false,
-        observaciones1: req.body || '',
-
-        re2: req.body || false,
-        observaciones2: req.body || '',
-
-        re3: req.body || false,
-        observaciones3: req.body || '',
-
-        re4: req.body || false,
-        observaciones4: req.body || '',
-
-        re5: req.body || false,
-        observaciones5: req.body || '',
-
-        noSeriePantografo: req.body || 0,
-        noSerieValvula: req.body || 0,
-
-        //AJUSTES DEL PANTOGRAFO
-        //ESFUERZO ESTATICO
-        eE1: req.body || '',
-        eE2: req.body || '',
-
-        //ESTI
-        esti: req.body || '',
-
-        //TIEMPO DE ELEVACION Y DESCENSO
-        ted1: req.body || '',
-        ted2: req.body || '',
-
-        ///DISTANCIA GANCHO TOPE 
-        dgt: req.body || '',
-
-        //ESFUERZO DE DISPARO
-        fd1: req.body || '',
-        fd2: req.body || '',
-
-        //ESTANQUEIDAD
-        voE: req.body || '',
-        observacionesE: req.body || '',
+        noInspeccion: req.body.noInspeccion || '',
+        kilometraje: req.body.kilometraje || 0,
+        horaInicio: req.body.horaInicio,
+        horaTerminacion: req.body.horaTerminacion,
+        fechaInicio: req.body.fechaInicio,
+        fechaTerminacion: req.body.fechaTerminacion,
+        
+        observacions1: req.body.observacions1,
+    
+        terminalTensionVF: req.body.terminalTensionVF,
+        terminalTensionL: req.body.terminalTensionL,
+        terminalTensionF: req.body.terminalTensionF,
+    
+        cubiertaPorcelanaVF: req.body.cubiertaPorcelanaVF,
+        cubiertaPorcelanaL: req.body.cubiertaPorcelanaL,
+        cubiertaPorcelanaF: req.body.cubiertaPorcelanaF,
+    
+        orificioGasVF: req.body.orificioGasVF,
+        orificioGasL: req.body.orificioGasL,
+        orificioGasF: req.body.orificioGasF,
+    
+        coneccionTierraVF: req.body.coneccionTierraVF,
+        coneccionTierraL: req.body.coneccionTierraL,
+        coneccionTierraF: req.body.coneccionTierraF,
+    
+        orificioLluviaVF: req.body.orificioLluviaVF,
+        orificioLluviaL: req.body.orificioLluviaL,
+        orificioLluviaF: req.body.orificioLluviaF,
+    
+        orificioFijacionVF: req.body.orificioFijacionVF,
+        orificioFijacionL: req.body.orificioFijacionL,
+        orificioFijacionF: req.body.orificioFijacionF,
+    
+        tornilleriaVF: req.body.tornilleriaVF,
+        tornilleriaL: req.body.tornilleriaL,
+        tornilleriaF: req.body.tornilleriaF,
+    
+        resistenciNLineal100: req.body.resistenciNLineal100,
+        resistenciNLineal200: req.body.resistenciNLineal200,
+    
+        observacions2: req.body.observacions2,    
 
         materialUtilizado: materiales,
+
+        equipoUtilizado: equipos
+
     };
     await mmmaM01.findByIdAndUpdate(id, { $set: MMMA_M01}, { new: true });
     res.json({
@@ -241,7 +92,7 @@ MMMA_M01.getStateCheckedApprovalByWorker = async (req, res) => {
 MMMA_M01.getAllMaintenenceMMMA_M01 = async (req, res) => {
     const maintenances = await mmmaM01.find({}, {
         "_id": 1, 
-        "unidad": 1, 
+        "noTren": 1, 
         "documentFormCurrentState.approvalByWorker.checked": 1, 
         "documentFormCurrentState.approvalBySupervisor.checked": 1,
         "documentFormCurrentState.approvalByMannager.checked": 1,
--- models/maintenence-mayor.js
+++ models/maintenence-mayor.js
@@ -5,9 +5,10 @@ var mmmaM01Schema = Schema({//esquema de la estructura que va a tener el formula
 
     date_created: { type: Date, required: true },
     //datos tren
-    noInspeccion: { type: String },
     noTren: { type: String },
     kilometraje: { type: String },
+    horaInicio: { type: String },
+    horaTerminacion: { type: String },
     fechaInicio: { type: String },
     fechaTerminacion: { type: String },
     
@@ -16,209 +17,61 @@ var mmmaM01Schema = Schema({//esquema de la estructura que va a tener el formula
     id_worker1: {type: String},
     operario2: { type: String, required: true},
     id_worker2: {type: String},
-    unidad: { type: Number, required: true },
 
-    colocarSm1: Boolean,
-    observacionesCs: String,
+    observacions1: String,
 
-    retirarTm1: Boolean,
-    observacionesRt: String,
+    terminalTensionVF: String,
+    terminalTensionL: String,
+    terminalTensionF: String,
 
-    desconectarMm1: Boolean,
-    observacionesDm: String,
+    cubiertaPorcelanaVF: String,
+    cubiertaPorcelanaL: String,
+    cubiertaPorcelanaF: String,
 
-    retirarTFm1: Boolean,
-    observacionesRtf: String,
+    orificioGasVF: String,
+    orificioGasL: String,
+    orificioGasF: String,
 
-    bajarPm1: Boolean,
-    observacionesBp: String,
+    coneccionTierraVF: String,
+    coneccionTierraL: String,
+    coneccionTierraF: String,
 
-    //2. MANTENIMIENTO DEL PANTOGRAFO
-    conjuntoAR: Boolean,
-    conjuntoAL: Boolean,
-    conjuntoAC: Boolean,
-    observacionesCam: String,
+    orificioLluviaVF: String,
+    orificioLluviaL: String,
+    orificioLluviaF: String,
 
-    conjuntoBR: Boolean,
-    conjuntoBL: Boolean,
-    conjuntoBE: Boolean,
-    conjuntoBC: Boolean,
-    observacionesCb: String,
+    orificioFijacionVF: String,
+    orificioFijacionL: String,
+    orificioFijacionF: String,
 
-    conjuntoBIR: Boolean,
-    conjuntoBIL: Boolean,
-    conjuntoBIE: Boolean,
-    conjuntoBIC: Boolean,
-    observacionesCbi: String,
+    tornilleriaVF: String,
+    tornilleriaL: String,
+    tornilleriaF: String,
 
-    conjuntoER: Boolean,
-    conjuntoEL: Boolean,
-    conjuntoEE: Boolean,
-    conjuntoEC: Boolean,
-    observacionesCe: String,
+    resistenciNLineal100: Number,
+    resistenciNLineal200: Number,
 
-    conjuntoBER: Boolean,
-    conjuntoBEL: Boolean,
-    conjuntoBEE: Boolean,
-    conjuntoBEC: Boolean,
-    observacionesCbe: String,
-
-    conjuntoCR: Boolean,
-    conjuntoCL: Boolean,
-    conjuntoCE: Boolean,
-    conjuntoCC: Boolean,
-    observacionesCc: String,
-
-    conjuntoAC: Boolean,
-    observacionesCa: String,
-
-    conjuntoMR: Boolean,
-    conjuntoML: Boolean,
-    conjuntoME: Boolean,
-    conjuntoMC: Boolean,
-    observacionesCm: String,
-
-    conjuntoMSR: Boolean,
-    conjuntoMSL: Boolean,
-    conjuntoMSC: Boolean,
-    observacionesCms: String,
-
-    conjuntoVR: Boolean,
-    conjuntoVL: Boolean,
-    conjuntoVE: Boolean,
-    conjuntoVC: Boolean,
-    observacionesCv: String,
-
-    //CONEXIONES FLEXIBLES
-    verificarE8re: Boolean,
-    observacionesVe8: String,
-
-    verificarECre: Boolean,
-    observacionesVec: String,
-
-    aplicarGre: Boolean,
-    observacionesAg: String,
-
-    colocarMCre: Boolean,
-    observacionesCmc: String,
-
-    aplicarPre: Boolean,
-    observacionesAp: String,
-
-    //ARQUET NUEVO
-    verificarVre: Boolean,
-    observacionesVv: String,
-
-    separacionBre: Boolean,
-    observacionesSb: String,
-
-    alizarAre: Boolean,
-    observacionesAa: String,
-
-    verificarTre: Boolean,
-    observacionesVt: String,
-
-    //MOTOR NEUMATICO(CON PRESION DE AIRE REGULADA)
-    presionAre: Boolean,
-    observacionesPa: String,
-
-    presionRre: Boolean,
-    observacionesPr: String,
-
-    recorridoPre: Boolean,
-    observacionesRp: String,
-
-    alimentarCre: Boolean,
-    observacionesAc: String,
-
-    //LUBRICACION
-    codigoCm: String,
-    descipcionCm: String,
-    reCm: Boolean,
-    observacionesCmec: String,
-
-    codigoCep: String,
-    descipcionCep: String,
-    reCep: Boolean,
-    observacionesCep: String,
-
-    codigoEn: String,
-    descipcionEn: String,
-    reEn: Boolean,
-    observacionesEn: String,
-
-    codigoPe: String,
-    descipcionPe: String,
-    rePe: Boolean,
-    observacionesPe: String,
-
-    //3. MONTAJE DE PANTOGRAFO
-    subirPm1: Boolean,
-    observacionesSp: String,
-
-    colocarTm1: Boolean,
-    observacionesCt: String,
-
-    conectarMm1: Boolean,
-    observacionesCm: String,
-
-    conectarCm1: Boolean,
-    observacionesCc: String,
-
-    realizarAm1: Boolean,
-    observacionesRa: String,
-
-    //PARES DE APRIETE
-    re1: Boolean,
-    observaciones1: String,
-
-    re2: Boolean,
-    observaciones2: String,
-
-    re3: Boolean,
-    observaciones3: String,
-
-    re4: Boolean,
-    observaciones4: String,
-
-    re5: Boolean,
-    observaciones5: String,
-
-    noSeriePantografo: Number,
-    noSerieValvula: Number,
-
-    //AJUSTES DEL PANTOGRAFO
-    //ESFUERZO ESTATICO
-    eE1: String,
-    eE2: String,
-
-    //ESTI
-    esti: String,
-
-    //TIEMPO DE ELEVACION Y DESCENSO
-    ted1: String,
-    ted2: String,
-
-    ///DISTANCIA GANCHO TOPE 
-    dgt: String,
-
-    //ESFUERZO DE DISPARO
-    fd1: String,
-    fd2: String,
-
-    //ESTANQUEIDAD
-    voE: String,
-    observacionesE: String,
+    observacions2: String,
 
     //material utilizado
     materialUtilizado: {
         numberOfMaterial: Number,    
         arr: [{
-            _id: String,
             code: String,
             description: String,
             voucherNumber: String,
             quantity: String,
+            unit: String
+        }]
+    },
+
+    //Equipo utilizado
+    equipoUtilizado: {
+        numberOfEquipo: Number,    
+        arr: [{
+            equipo: String,
+            marca: String,
+            inventario: String
         }]
     },
 
